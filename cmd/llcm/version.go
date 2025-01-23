41fa22485e639da57d27ccfa8884d4226c95d989
nekrassov01 <unclek62@gmail.com>
2025-01-23 16:12:31 +0900

    chore: update release settings

--- cmd/llcm/version.go
+++ cmd/llcm/version.go
@@ -5,13 +5,13 @@ import "fmt"
 // Version is the current version of llcm.
 const Version = "0.0.4"
 
-// Revision is the git revision of llcm.
-var Revision = ""
+// Commit is the git revision of llcm.
+var Commit = ""
 
-// version returns the version of llcm.
+// version returns the version and revision of llcm.
 func version() string {
-	if Revision == "" {
+	if Commit == "" {
 		return Version
 	}
-	return fmt.Sprintf("%s (revision: %s)", Version, Revision)
+	return fmt.Sprintf("%s (revision: %s)", Version, Commit)
 }
