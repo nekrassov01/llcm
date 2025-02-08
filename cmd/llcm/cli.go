fe3afc25e1c11e15abf4ef06d8b5573aaf164772
nekrassov01 <unclek62@gmail.com>
2025-02-09 01:33:17 +0900

    chore: fix text

--- cmd/llcm/cli.go
+++ cmd/llcm/cli.go
@@ -54,7 +54,7 @@ func newApp(w, ew io.Writer) *app {
 		Aliases:     []string{"r"},
 		Usage:       "set target regions",
 		Value:       cli.NewStringSlice(llcm.DefaultRegions...),
-		DefaultText: "all regions with no opt-in required",
+		DefaultText: "all regions with no opt-in",
 	}
 	a.filter = &cli.StringSliceFlag{
 		Name:    "filter",
