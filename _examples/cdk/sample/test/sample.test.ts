65cfa6622831d53897ba0c287cba7d2712b587b8
nekrassov01 <unclek62@gmail.com>
2025-03-07 17:10:34 +0900

    chore: add cdk test

--- _examples/cdk/sample/test/sample.test.ts
+++ _examples/cdk/sample/test/sample.test.ts
@@ -1,17 +1,31 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Sample from '../lib/sample-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { SampleStack } from "../lib/sample-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/sample-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Sample.SampleStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+const app = new cdk.App();
+const stack = new SampleStack(app, "SampleStack");
+const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe("Snapshot Tests", () => {
+  test("Snapshot test", () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe("Fine-grained Assertions Tests", () => {
+  test("Lambda Function created", () => {
+    template.resourceCountIs("AWS::Lambda::Function", 2); // including the custom resource
+  });
+  test("Lambda Version created", () => {
+    template.resourceCountIs("AWS::Lambda::Version", 1);
+  });
+  test("Lambda Alias created", () => {
+    template.resourceCountIs("AWS::Lambda::Alias", 1);
+  });
+  test("Scheduler Schedule created", () => {
+    template.resourceCountIs("AWS::Scheduler::Schedule", 1);
+  });
+  test("IAM Role created", () => {
+    template.resourceCountIs("AWS::IAM::Role", 3); // including the custom resource
+  });
 });
