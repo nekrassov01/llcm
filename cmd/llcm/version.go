cc910bd90dcc720fdcf8f89949f7333e11a7b7d4
nekrassov01 <unclek62@gmail.com>
2026-01-20 13:26:39 +0900

    bump up version to 0.0.29

--- cmd/llcm/version.go
+++ cmd/llcm/version.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // version is the current version of llcm.
-const version = "0.0.28"
+const version = "0.0.29"
 
 // revision is the git revision of llcm.
 var revision = ""
