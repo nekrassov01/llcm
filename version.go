65c8489c667ea880aad2baf8836ebbf8a19b8f58
nekrassov01 <unclek62@gmail.com>
2026-04-20 22:22:56 +0900

    refactor: update cli and versioning

---
+++ version.go
@@ -0,0 +1,17 @@
+package llcm
+
+import "fmt"
+
+// version is the current version.
+const version = "0.0.29"
+
+// revision is the git revision.
+var revision = ""
+
+// Version returns the version and revision.
+func Version() string {
+	if revision == "" {
+		return version
+	}
+	return fmt.Sprintf("%s (revision: %s)", version, revision)
+}
